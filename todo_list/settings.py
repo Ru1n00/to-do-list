03c83136531cf6fb9df00bede88469886c356656
Aja1ra <ajairakamerid@gmail.com>
2022-04-12 04:59:18 +0600

    added to allowed host

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ojfm0+)c&d*$+cmhw%5mz8!f-$mn)e76b8mt*)dq_n3jb347z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'warm-woodland-39202']
+ALLOWED_HOSTS = ['127.0.0.1', 'warm-woodland-39202.herokuapp.com']
 
 
 # Application definition
